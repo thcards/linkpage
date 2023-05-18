264c998b2e7b7af28226d0f855143f98043cf012
Thiago Cardoso <129462660+thcards@users.noreply.github.com>
2023-05-18 03:48:15 -0300

    Delete games directory

--- games/script.js
+++
@@ -1,5 +0,0 @@
-let interruptor = document.getElementById('interruptor')
-let root = document.documentElement
-interruptor.addEventListener("click", ()=>  {
-    root.classList.toggle ('tema-claro')
-})
